8883b517d1f7c28c4b8834471626d50890e80621
greglab74 <greg.labbe08@gmail.com>
2018-06-15 16:17:44 +0200

    greg

--- js/timer.js
+++ js/timer.js
@@ -1,19 +1,19 @@
-var dixie=0 //initialisation minute,seconde,dixièmes à 0
+var heure=0 //initialisation minute,seconde,dixièmes à 0
 var secon=0
 var minut=0
 
 function chrono(){
-dixie++; //incrémentation des dixièmes de 1
-if (dixie>9){dixie=0;secon++} //si les dixièmes > 9,on les remet à 0 et on incrémente les secondes de 1
-if (secon>59){secon=0;minut++} //si les secondes > 59,on les réinitialise à 0 et on incrémente les minutes de 1
-document.affiche.dix.value=" "+dixie //on affiche les dixièmes
-document.affiche.sec.value=" "+secon //on affiche les secondes
-document.affiche.min.value=" "+minut //on affiche les minutes
+secon++; //incrémentation des dixièmes de 1
+if (secon>59){dixie=0;minut++} //si les secondes > 59,on les réinitialise à 0 et on incrémente les minutes de 1
+if (minut>59){minut=0;heure++} //si les minutes > 59,on les réinitialise à 0 et on incrémente les heures de 1
+document.getElementById('heure').innerHTML=" "+heure //on affiche les heures
+document.getElementById('secon').innerHTML=" "+secon //on affiche les secondes
+document.getElementById('minut').innerHTML=" "+minut //on affiche les minutes
 
 }
 function start() {
-var begin = setInterval(chrono,100) //la fonction start s'execute tous les disièmes de secondes
+var begin = setInterval(chrono,1000) //la fonction start s'execute toutes les secondes
 }
 function stop() {
-  clearsetInterval(begin) //arrête la fonction start
+  clearsetInterval(start) //arrête la fonction start
 }
