f2d5bc29896bf5964abc030565bdcf9f033d5284
greglab74 <39262246+greglab74@users.noreply.github.com>
2018-06-16 15:45:36 +0200

    Update timer.js

--- js/timer.js
+++ js/timer.js
@@ -6,7 +6,7 @@ function chrono(){
 dixie++; //incrémentation des dixièmes de 1
 if (dixie>9){dixie=0;secon++} //si les dixièmes > 9,on les remet à 0 et on incrémente les secondes de 1
 if (secon>59){secon=0;minut++} //si les secondes > 59,on les réinitialise à 0 et on incrémente les minutes de 1
-document.getElementById('dixieme').innerHTML=" "+dixie //on affiche les dixièmes
+document.getElementById('dixieme').innerHTML=" "+dixie //on affiche les dixièmes à l'intérieur de notre ID "dixieme" dans notre fichier HTML
 document.getElementById('seconde').innerHTML=" "+secon //on affiche les secondes
 document.getElementById('minute').innerHTML=" "+minut //on affiche les minutes
 
