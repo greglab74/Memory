d2726f03aa1bff39de21108168d00e1807e2e797
belvoixnicolas <belvoixnicolas1997@gmail.com>
2018-06-18 10:25:27 +0200

    d

--- js/timer.js
+++ js/timer.js
@@ -4,17 +4,17 @@ var minut=0;
 
 function chrono(){
 dixie++; //incrémentation des dixièmes de 1
-if (dixie>9){dixie=0;secon++} //si les dixièmes > 9,on les remet à 0 et on incrémente les secondes de 1
-if (secon>59){secon=0;minut++} //si les secondes > 59,on les réinitialise à 0 et on incrémente les minutes de 1
-document.getElementsByClassName('dixieme').innerHTML=" "+dixie //on affiche les dixièmes à l'intérieur de notre ID "dixieme" dans notre fichier HTML
-document.getElementsByClassName('seconde').innerHTML=" "+secon //on affiche les secondes
-document.getElementsByClassName('minute').innerHTML=" "+minut //on affiche les minutes
-
+if (dixie>9){dixie=0;secon++;} //si les dixièmes > 9,on les remet à 0 et on incrémente les secondes de 1
+if (secon>59){secon=0;minut++;} //si les secondes > 59,on les réinitialise à 0 et on incrémente les minutes de 1
+document.getElementById('dixieme').innerHTML=" "+dixie; //on affiche les dixièmes à l'intérieur de notre ID "dixieme" dans notre fichier HTML
+document.getElementById('seconde').innerHTML=" "+secon; //on affiche les secondes
+document.getElementById('minute').innerHTML=" "+minut; //on affiche les minutes
+console.log(dixie);
 }
-var begin
+var begin;
 function start() {
-begin = setInterval(chrono,100)//la fonction start s'execute tous les disièmes de secondes
+begin = setInterval(chrono,100); //la fonction start s'execute tous les disièmes de secondes
 }
 function stop() {
-  clearInterval(begin) //arrête la fonction start
+  clearInterval(begin); //arrête la fonction start
 }
